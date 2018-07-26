e206131aacafd7851fabb116f3413d7e9fec3209
ruby -v <fernando_0970@outlook.es>
2018-07-26 17:07:01 -0500

    save button working

--- app/routes/auth/book-edit.js
+++ app/routes/auth/book-edit.js
@@ -3,5 +3,17 @@ import Route from '@ember/routing/route';
 export default Route.extend({
     model(params) {
         return this.store.findRecord('music-book', params.id);
+    },
+    actions: {
+        saveBook(book) {
+            book.save();
+            this.transitionTo('auth.book-list');
+        },
+
+        deleteBook(book) {
+            book.destroyRecord().then(() => {
+                this.transitionTo('auth.book-list');
+            })
+        }
     }
 });
